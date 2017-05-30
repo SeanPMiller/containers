48619b27cd07ff2bacafb78d0f4f426192ef6863
Sean P. Miller <unvexis@yahoo.com>
2017-05-29 18:01:06 -0700

    Begin documenting sequence.

--- src/main/java/org/seanpatrickmiller/containers/fingertree/Sequence.java
+++ src/main/java/org/seanpatrickmiller/containers/fingertree/Sequence.java
@@ -12,6 +12,9 @@ import org.seanpatrickmiller.containers.util.Func;
 import org.seanpatrickmiller.containers.util.Identity;
 import org.seanpatrickmiller.containers.util.Monoids;
 
+/**
+ * Indexed sequence.
+ */
 public final class Sequence<A> implements java.lang.Iterable<A>
 {
     // This sequence's actual representation.
@@ -60,11 +63,19 @@ public final class Sequence<A> implements java.lang.Iterable<A>
         return this.tree.split(f);
     }
 
+    /**
+     * Is this sequence empty?
+     * @return true if this sequence is empty; otherwise, false.
+     */
     public boolean isEmpty()
     {
         return this.tree.isEmpty();
     }
 
+    /**
+     * Get the first element in this sequence.
+     * @return The first element in this sequence.
+     */
     public A head()
     {
         return this.tree.head();
@@ -75,6 +86,10 @@ public final class Sequence<A> implements java.lang.Iterable<A>
         return new Sequence<A>(this.tree.tail(), this);
     }
 
+    /**
+     * Get the last element in this sequence.
+     * @return The last element in this sequence.
+     */
     public A last()
     {
         return this.tree.rhead();
@@ -97,22 +112,42 @@ public final class Sequence<A> implements java.lang.Iterable<A>
         return new SequenceSpliterator<A>(this.tree);
     }
 
+    /**
+     * Push an element onto the front of this sequence.
+     * @param a The element to push.
+     * @return A new sequence including the given element.
+     */
     public Sequence<A> pushFront(final A a)
     {
         return new Sequence<A>(this.tree.pushFront(a), this);
     }
 
+    /**
+     * Push an element onto the back of this sequence.
+     * @param a The element to push.
+     * @return A new sequence including the given element.
+     */
     public Sequence<A> pushBack(final A a)
     {
         return new Sequence<A>(this.tree.pushBack(a), this);
     }
 
+    /**
+     * Append another sequence to the back of this sequence.
+     * @param other The sequence to append to this sequence.
+     * @return A new sequence including both sequences.
+     */
     public Sequence<A> append(final Sequence<A> other)
     {
         return new Sequence<A>(this.tree.append(other.tree), this);
     }
 
     // TODO: support map : [A] * (A -> B) -> [B]?
+    /**
+     * Apply a function to each element in this sequence.
+     * @param f The function to apply.
+     * @return A new sequence containing the results.
+     */
     public Sequence<A> map(final Func<A, A> f)
     {
         return new Sequence<A>(this.tree.map(f), this);
@@ -133,15 +168,21 @@ public final class Sequence<A> implements java.lang.Iterable<A>
         return new Sequence<A>(this.tree.reverse(this.identity), this);
     }
 
+    /**
+     * Get the element at the given index.
+     * @param index
+     * @return
+     */
     public A at(final int index)
     {
         return this.split(index).right.head();
     }
 
     /**
-     * Insert an item before the given index.
+     * Insert an item before the element at the given index.
      * @param index The index before which to insert.
      * @param a The element to insert.
+     * @return
      */
     public Sequence<A> insert(final int index, final A a)
     {
@@ -149,22 +190,41 @@ public final class Sequence<A> implements java.lang.Iterable<A>
         return new Sequence(splat.left.pushBack(a).append(splat.right), this);
     }
 
+    /**
+     * Delete the item at the given index.
+     * @param index The index of the element to delete.
+     * @return
+     */
     public Sequence<A> delete(final int index)
     {
         FingerTree.SplitPair<java.lang.Integer, A> splat = this.split(index);
         return new Sequence(splat.left.append(splat.right.tail()), this);
     }
 
+    /**
+     * Get this sequence's size.
+     * @return This sequence's size.
+     */
     public java.lang.Integer size()
     {
         return this.tree.measure();
     }
 
+    /**
+     * Get elements from the front of this sequence.
+     * @param count How many elements to take.
+     * @return
+     */
     public Sequence<A> take(final int count)
     {
         return new Sequence(this.split(count).left, this);
     }
 
+    /**
+     * Remove elements from the front of this sequence.
+     * @param count How many elements to drop.
+     * @return
+     */
     public Sequence<A> drop(final int count)
     {
         return new Sequence(this.split(count).right, this);
